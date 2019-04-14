bc4e005e0c2c973f7c1fe845e3dcdbcc0d91bc59
Vladyslav <ganushkevychv@gmail.com>
2019-04-14 20:54:58 +0200

    blog created

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Blog</h1>
+    <h1>Home page</h1>
     
   </Layout>
 )
