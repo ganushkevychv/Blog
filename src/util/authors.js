005269348311a380b5b2253135cb7e973242a143
Vladyslav <ganushkevychv@gmail.com>
2020-05-04 12:43:28 +0200

    img was changed in util/authors

--- src/util/authors.js
+++ src/util/authors.js
@@ -1,7 +1,7 @@
 const authors = [
     {
         name: ' Vladyslav Ganushkevych',
-        imageUrl: 'me.jpg',
+        imageUrl: 'portfolioFoto.jpg',
         bio: 'Front-end developer.I am 33 years old and my passion is programing and aviation! I am flexible with my work timings and can work in a team.',
         github: 'https://github.com/ganushkevychv/',
         linkedin: 'https://www.linkedin.com/in/vladyslav-ganushkevych/',
